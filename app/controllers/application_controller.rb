4c9313f8287dfef0b9286a73e4d53ab1b3063184
Ushinji <siguma971@gmail.com>
2018-01-01 18:38:54 +0900

    Auth認証によるログイン機能とログアウト機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?,
+
+  private
+  def logged_in?
+    session[:user_id].present?
+  end
+  def logout
+    session[:user_id] = nil
+  end
 end
---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,10 @@
+describe 'POST #create' do
+  context 'twitterのログインに成功したら'
+  it 'rootへリダイレクトされること' do
+    
+  end
+
+  it '「ログインしました」とメッセージが表示されること'　do
+  
+  end
+end
