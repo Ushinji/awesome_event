3b5812d062a88058662dce752be8a2052754b2a7
Ushinji <siguma971@gmail.com>
2018-01-04 12:32:09 +0900

    ログイン無しにイベント作成画面のアクセスをできなくした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :logged_in?,
+  helper_method :logged_in?, :current_user, :authenticate
 
   private
   def logged_in?
@@ -10,9 +10,16 @@ class ApplicationController < ActionController::Base
   def logout
     session[:user_id] = nil
   end
-  
+
   def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    unless logged_in?
+      flash[:danger] = 'ログインをしてください'
+      redirect_to root_path
+    end
   end
 end
