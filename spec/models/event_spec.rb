ea1359ee674d5262b780d64715ca84f7401a8878
Ushinji <siguma971@gmail.com>
2018-01-04 17:14:27 +0900

    Eventモデルのテストの作成

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,13 +1,51 @@
 require "rails_helper"
 
 describe "Event" do
-  it "test" do
-    event = FactoryBot.build(:event)
-    event.name = 'TEST'
-    event.place = 'PLACE'
-    event.content = 'CONTENT'
-    event.start_time = Time.current
-    event.end_time = Time.current + 1
-    expect(event).to be_valid
+  describe "#name" do
+    context "空白のとき" do
+      it "validではないこと" do
+        event = Event.new(name: '')
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
+
+  describe "#place" do
+    context "空白のとき" do
+      it "validではないこと" do
+        event = Event.new(place: '')
+        event.valid?
+        expect(event.errors[:place]).to be_present
+      end
+    end
+  end
+
+  describe "#start_time" do
+    context "空白のとき" do
+      it "validではないこと" do
+        event = Event.new(start_time: '')
+        event.valid?
+        expect(event.errors[:start_time]).to be_present
+      end
+    end
+
+    context "#end_timeより遅い" do
+      it "validではないこと" do
+        event = Event.new(start_time: Time.current, end_time: Time.current - 1)
+        event.valid?
+        expect(event.errors[:start_time]).to be_present
+      end
+    end
+  end
+
+  describe "#end_time" do
+    context "空白のとき" do
+      it "validではないこと" do
+        event = Event.new(end_time: '')
+        event.valid?
+        expect(event.errors[:end_time]).to be_present
+      end
+    end
   end
 end
