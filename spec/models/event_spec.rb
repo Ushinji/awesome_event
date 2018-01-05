d313894781c3de6aebfb4def12815e7935a0a229
Ushinji <siguma971@gmail.com>
2018-01-05 09:02:53 +0900

    modeloのバリデーションテストの修正

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,46 +1,21 @@
 require "rails_helper"
 
 describe "Event" do
-  describe "#name" do
+  context "presenceのバリデーション" do
     subject{ build(:event) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:place) }
+    it { is_expected.to validate_presence_of(:start_time) }
+    it { is_expected.to validate_presence_of(:end_time) }
+    it { is_expected.to validate_presence_of(:content) }
     it { is_expected.to validate_presence_of(:name) }
   end
 
-  describe "#place" do
-    context "空白のとき" do
-      it "validではないこと" do
-        event = Event.new(place: '')
-        event.valid?
-        expect(event.errors[:place]).to be_present
-      end
-    end
-  end
-
-  describe "#start_time" do
-    context "空白のとき" do
-      it "validではないこと" do
-        event = Event.new(start_time: '')
-        event.valid?
-        expect(event.errors[:start_time]).to be_present
-      end
-    end
-
-    context "#end_timeより遅い" do
-      it "validではないこと" do
-        event = Event.new(start_time: Time.current, end_time: Time.current - 1)
-        event.valid?
-        expect(event.errors[:start_time]).to be_present
-      end
-    end
-  end
-
-  describe "#end_time" do
-    context "空白のとき" do
-      it "validではないこと" do
-        event = Event.new(end_time: '')
-        event.valid?
-        expect(event.errors[:end_time]).to be_present
-      end
+  context "start_timeがend_timeより遅いとき" do
+    it "validではないこと" do
+      event = Event.new(start_time: Time.current, end_time: Time.current - 1)
+      event.valid?
+      expect(event.errors[:start_time]).to be_present
     end
   end
 end
