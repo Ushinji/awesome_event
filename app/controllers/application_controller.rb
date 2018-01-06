f00647e9828f7788e845102dd88ad7b5fc20b776
Ushinji <siguma971@gmail.com>
2018-01-06 16:09:18 +0900

    NotFoundの例外捕縛と404ページ表示を行うようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_user, :authenticate
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
   private
   def logged_in?
     session[:user_id].present?
--- spec/controllers/events/show_spec.rb
+++ spec/controllers/events/show_spec.rb
@@ -18,5 +18,17 @@ describe EventsController do
         expect(response).to render_template :show
       end
     end
+
+    context '存在しないEventの場合' do
+      it 'レスポンスが、404/NotFoundを返すこと' do
+        get :show, params: { id: 0 }
+        expect(response).to have_http_status :not_found
+      end
+
+      it '404.htmlを表示すること' do
+        get :show, params: { id: 0 }
+        expect(response).to render_template(:file => Rails.root.join('public/404.html').to_s)
+      end
+    end
   end
 end
